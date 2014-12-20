e3cd530889bffb9f80ed432bfd904e66c4bdd646
neillsilva <blackvector@gmail.com>
2014-12-20 14:58:27 -0500

    MediaPage model validations

--- spec/models/media_page_spec.rb
+++ spec/models/media_page_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe MediaPage, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:media_page).should be_valid
+  end
+
+  it "is invalid without a name" do
+    FactoryGirl.build(:media_page, name: nil).should_not be_valid
+  end
+
+  it "is invalid without content" do
+    FactoryGirl.build(:media_page, content: nil).should_not be_valid
+  end
 end
