027c9ee2170e93675816ba0d3f9260978f2b05e1
neillsilva <blackvector@gmail.com>
2014-12-20 16:43:47 -0500

    Trouble with form_for routes

--- app/helpers/media_pages_helper.rb
+++ app/helpers/media_pages_helper.rb
@@ -1,2 +1,9 @@
 module MediaPagesHelper
+    def submit_content_btn
+        if params[:action] == 'edit'
+            return "Edit Media"
+        else
+            return "Add Media"
+        end
+    end
 end
